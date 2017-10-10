7a25ffa7c1fd707b6ce306bd773fce770b698285
BugleJones <spencerjhamilton@gmail.com>
2017-10-10 19:02:28 +0000

    Updated stores table with new and fun information

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,10 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+burb_store = Store.create name: "Burnaby", annual_revenue: 30000, mens_apparel: true, womens_apparel: true
+rich_store = Store.create name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true
+gas_store = Store.create name: "Gastown", annual_revenue: 190000,  mens_apparel: false, womens_apparel: false
+
+all_stores = Store.all
+
+puts "We have #{all_stores.count} stores"
