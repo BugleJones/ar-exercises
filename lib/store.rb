ae6cf228fbc9c6377c13f6efc5a8cdc3cca148f9
BugleJones <spencerjhamilton@gmail.com>
2017-10-11 00:18:54 +0000

    Added password validation and and anti-store destruction

--- lib/store.rb
+++ lib/store.rb
@@ -5,6 +5,7 @@ class Store < ActiveRecord::Base
     validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     
     validate :need_womens_or_mens_apparel
+    before_destroy :ensure_has_no_employees
 
     private
     def need_womens_or_mens_apparel
@@ -13,5 +14,12 @@ class Store < ActiveRecord::Base
         errors.add(:womens_apparel, "Need atleast mens or womens apparel")
         end
     end
+
+    def ensure_has_no_employees
+        if self.employees.any?
+          errors[:base] << "cannot delete store that has employees"
+          return false
+        end
+      end
 
 end
